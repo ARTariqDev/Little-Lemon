de2692a5847b6f0e512854ff4fb0f1138dc38517
AhYes-a-Username <tariqmahmood.dmp@gmail.com>
2024-04-15 02:47:09 +0500

    Added new feature X

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Header from './Header.js';
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <>
+    <Header />
+    <main>
+
+    </main>
+    <footer>
+    </footer>
+    </>
   );
 }
 
